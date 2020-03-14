90986e45534813c5235174906ca8dc4e89be0f2d
rsimone <simone.rebora81@gmail.com>
2020-03-14 19:40:42 +0100

    First commit

--- Josefine_Mutzenbacher_imposters.R
+++ Josefine_Mutzenbacher_imposters.R
@@ -1,35 +1,42 @@
 library(stylo)
 
+# choose distance for analysis
 chosen_distance <- "wurzburg"
-chosen_mfw <- 2000
 
-### read document-term matrixes
+# find document-term matrixes
 all_imposters_files <- list.files("corpus/Imposters", pattern = ".csv", full.names = T)
+# extract names of candidates
 all_candidate_names <- gsub(pattern = "corpus/Imposters/Imposters_|.csv", replacement = "", x = all_imposters_files)
 
-### separate imposter of interest
+# run analysis for each candidate
 for(i in 1:length(all_candidate_names)){
   
   my_candidate <- all_candidate_names[i]
   
+  # read data from d-t matrix
   data <- read.csv(all_imposters_files[i], stringsAsFactors = F, row.names = 1)
   
+  # get titles of all texts
   all_texts <- rownames(data)
+
+  # get names of all authors
   all_authors <- strsplit(all_texts, "_")
   all_authors <- unlist(lapply(all_authors, function(x) x[1]))
   all_authors <- unique(all_authors)
   
+  # isolate imposters
   my_imposters <- all_authors[-c(1,2)]
   
-  filename <- paste("Imposters_full_attribution_", my_candidate, ".txt", sep = "")
-  
+  # identify test text (always the first here) and texts of candidate author
   test_id <- 1
   candidate_id <- which(grepl(pattern = my_candidate, x = all_texts))
   
   # use imposters on selected candidate
-  data <- as.matrix(data)
+  data <- as.matrix(data) # dataframe has to be trasformed into matrix to be read by stylo
   imposters_results <- imposters(reference.set = data[-c(test_id, candidate_id),], test = data[test_id,], candidate.set = data[candidate_id,], distance = chosen_distance)
-    
+
+  # save results
+  filename <- paste("Imposters_full_attribution_", my_candidate, ".txt", sep = "")  
   cat(my_candidate, "probability:", imposters_results, "\nDistance:", chosen_distance, " MFW:", dim(data)[2], "\nImposters:", my_imposters, file = filename, sep = " ")
 
 }
